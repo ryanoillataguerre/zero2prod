b4749f0ec05dbb4256b44eab423193c07b905f13
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2022-07-10 16:57:38 -0700

    Health check route added

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::net::TcpListener;
+
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check(_: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("localhost:8080").expect("Failed to bind to port");
+    run(listener)?.await
 }
